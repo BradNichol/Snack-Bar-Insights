c6cd34b2b6961ad8950ff86c28633316cf9bf429
bradley.nichol <bradleynichol@live.co.uk>
2020-11-23 17:54:36 +0000

    brands entity class updated.

--- src/main/java/uk/co/bradleynichol/SnackBarInsights/entity/Brands.java
+++ src/main/java/uk/co/bradleynichol/SnackBarInsights/entity/Brands.java
@@ -1,8 +1,36 @@
 package uk.co.bradleynichol.SnackBarInsights.entity;
 
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "brands")
 public class Brands {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private String Id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Brands() {
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
