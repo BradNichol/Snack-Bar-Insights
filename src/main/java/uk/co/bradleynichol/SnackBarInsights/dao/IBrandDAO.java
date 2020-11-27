e3683feb0d0752a9b73b540b7784362109e9f92f
bradley.nichol <bradleynichol@live.co.uk>
2020-11-27 18:15:50 +0000

    Brand DAO and interface created.

---
+++ src/main/java/uk/co/bradleynichol/SnackBarInsights/dao/IBrandDAO.java
@@ -0,0 +1,7 @@
+package uk.co.bradleynichol.SnackBarInsights.dao;
+
+import uk.co.bradleynichol.SnackBarInsights.entity.Brand;
+
+public interface IBrandDAO {
+    void addBrand(Brand brand);
+}
---
+++ src/main/java/uk/co/bradleynichol/SnackBarInsights/dao/IBrandDAOImpl.java
@@ -0,0 +1,11 @@
+package uk.co.bradleynichol.SnackBarInsights.dao;
+
+import uk.co.bradleynichol.SnackBarInsights.entity.Brand;
+
+public class IBrandDAOImpl implements IBrandDAO {
+
+    @Override
+    public void addBrand(Brand brand) {
+
+    }
+}
