0b63db5dd5d26784201d032b80e147866ad7a676
bradley.nichol <bradleynichol@live.co.uk>
2020-12-01 18:24:57 +0000

    entitymanager added to DAO and persist method implemented.

--- src/main/java/uk/co/bradleynichol/SnackBarInsights/dao/BrandDAOImpl.java
+++ src/main/java/uk/co/bradleynichol/SnackBarInsights/dao/BrandDAOImpl.java
@@ -3,14 +3,19 @@ package uk.co.bradleynichol.SnackBarInsights.dao;
 import org.springframework.stereotype.Repository;
 import uk.co.bradleynichol.SnackBarInsights.entity.Brand;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
 @Transactional
 @Repository
 public class BrandDAOImpl implements IBrandDAO {
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @Override
     public void addBrand(Brand brand) {
-
+        entityManager.persist(brand);
     }
 }
