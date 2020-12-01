911d9c521bb66f1ce6f96af53b2ccf18aaad880c
bradley.nichol <bradleynichol@live.co.uk>
2020-12-01 18:21:07 +0000

    Method implementation.
    service injection into controller.

--- src/main/java/uk/co/bradleynichol/SnackBarInsights/controller/BrandsController.java
+++ src/main/java/uk/co/bradleynichol/SnackBarInsights/controller/BrandsController.java
@@ -1,12 +1,22 @@
 package uk.co.bradleynichol.SnackBarInsights.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import uk.co.bradleynichol.SnackBarInsights.service.BrandServiceImpl;
 
 @RestController
 @RequestMapping("/api/brands")
 public class BrandsController {
+
+    @Autowired
+    private BrandServiceImpl brandService;
+
+    //@PostMapping("/add")
+    //public ResponseEntity<Void> add
 
 
 
--- src/main/java/uk/co/bradleynichol/SnackBarInsights/service/BrandServiceImpl.java
+++ src/main/java/uk/co/bradleynichol/SnackBarInsights/service/BrandServiceImpl.java
@@ -1,13 +1,19 @@
 package uk.co.bradleynichol.SnackBarInsights.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import uk.co.bradleynichol.SnackBarInsights.dao.IBrandDAO;
 import uk.co.bradleynichol.SnackBarInsights.entity.Brand;
 
 @Service
 public class BrandServiceImpl implements IBrandService {
 
+    @Autowired
+    private IBrandDAO brandDAO;
+
     @Override
     public boolean addBrand(Brand brand) {
-        return false;
+        brandDAO.addBrand(brand);
+        return true;
     }
 }
