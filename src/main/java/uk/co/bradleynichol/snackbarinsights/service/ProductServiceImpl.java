6ec6e8a0357f7923d50f5c92c4a219ce35e478e4
bradley.nichol <bradleynichol@live.co.uk>
2020-12-05 12:41:43 +0000

    Product service updated with injected DAO and implemented.

--- src/main/java/uk/co/bradleynichol/snackbarinsights/service/ProductServiceImpl.java
+++ src/main/java/uk/co/bradleynichol/snackbarinsights/service/ProductServiceImpl.java
@@ -1,29 +1,38 @@
 package uk.co.bradleynichol.snackbarinsights.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import uk.co.bradleynichol.snackbarinsights.dao.IProductDAO;
 import uk.co.bradleynichol.snackbarinsights.entity.Product;
 
 @Service
 public class ProductServiceImpl implements IProductService {
 
+    private final IProductDAO productDAO;
+
+    @Autowired
+    public ProductServiceImpl(IProductDAO productDAO) {
+        this.productDAO = productDAO;
+    }
 
     @Override
     public boolean addProduct(Product product) {
-        return false;
+        productDAO.addProduct(product);
+        return true;
     }
 
     @Override
     public Product getProductById(String productId) {
-        return null;
+        return productDAO.getProductById(productId);
     }
 
     @Override
     public void updateProduct(Product product) {
-
+        productDAO.updateProduct(product);
     }
 
     @Override
     public void deleteProduct(String productId) {
-
+        productDAO.deleteProduct(productId);
     }
 }
