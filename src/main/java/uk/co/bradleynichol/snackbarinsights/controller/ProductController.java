c283c44b668e73e7e8e2d9ee45d4e55eb5ec9c7b
bradley.nichol <bradleynichol@live.co.uk>
2020-12-05 13:07:21 +0000

    Product controller updated with Post mapping.

--- src/main/java/uk/co/bradleynichol/snackbarinsights/controller/ProductController.java
+++ src/main/java/uk/co/bradleynichol/snackbarinsights/controller/ProductController.java
@@ -1,11 +1,34 @@
 package uk.co.bradleynichol.snackbarinsights.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+import uk.co.bradleynichol.snackbarinsights.entity.Product;
+import uk.co.bradleynichol.snackbarinsights.service.ProductServiceImpl;
 
 @RestController
 @RequestMapping("/api/product")
 public class ProductController {
 
+    private ProductServiceImpl productService;
 
+    @Autowired
+    public ProductController(ProductServiceImpl productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Void> addProduct(@RequestBody Product product, UriComponentsBuilder builder) {
+        boolean flag = productService.addProduct(product);
+        if (!flag) return new ResponseEntity<>(HttpStatus.CONFLICT);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(builder.path("api/product").buildAndExpand(product.getId()).toUri());
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
 }
