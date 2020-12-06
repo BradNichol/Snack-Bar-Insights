10ee0c21da2cde995d770ca8a3cf473ed49db2ca
bradley.nichol <bradleynichol@live.co.uk>
2020-12-06 00:33:53 +0000

    ScraperResource entity created.

---
+++ src/main/java/uk/co/bradleynichol/snackbarinsights/entity/ScraperResource.java
@@ -0,0 +1,57 @@
+package uk.co.bradleynichol.snackbarinsights.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "scraper_resources")
+public class ScraperResource {
+
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "product_id", nullable = false)
+    private String productId;
+
+    @Column(name = "url")
+    private String url;
+
+    @Column(name = "xpath")
+    private String xPath;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getxPath() {
+        return xPath;
+    }
+
+    public void setxPath(String xPath) {
+        this.xPath = xPath;
+    }
+}
