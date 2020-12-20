e9fd0ba9669a177a9abf709c78d27c97d3ae48d0
Bradley Nichol <bradleynichol@live.co.uk>
2020-12-20 20:18:50 +0000

    Get mapping added to Product controller.

--- src/main/java/uk/co/bradleynichol/snackbarinsights/controller/ProductController.java
+++ src/main/java/uk/co/bradleynichol/snackbarinsights/controller/ProductController.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 import uk.co.bradleynichol.snackbarinsights.entity.Product;
 import uk.co.bradleynichol.snackbarinsights.service.ProductServiceImpl;
@@ -30,5 +27,11 @@ public class ProductController {
         HttpHeaders headers = new HttpHeaders();
         headers.setLocation(builder.path("api/product").buildAndExpand(product.getId()).toUri());
         return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable("id") String id) {
+        Product product = productService.getProductById(id);
+        return new ResponseEntity<>(product, HttpStatus.OK);
     }
 }
