48943351569e96bc6a2e97e2861d2365a345a35f
Bradley Nichol <bradleynichol@live.co.uk>
2021-01-02 18:10:35 +0000

    Product Price controller created.

---
+++ src/main/java/uk/co/bradleynichol/snackbarinsights/controller/ProductPriceController.java
@@ -0,0 +1,38 @@
+package uk.co.bradleynichol.snackbarinsights.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+import uk.co.bradleynichol.snackbarinsights.entity.ProductPrice;
+import uk.co.bradleynichol.snackbarinsights.service.IProductPriceService;
+
+@RestController
+@RequestMapping("api/price")
+public class ProductPriceController {
+
+    private IProductPriceService productPriceService;
+
+    @Autowired
+    public ProductPriceController(IProductPriceService productPriceService) {
+        this.productPriceService = productPriceService;
+    }
+
+
+    @PostMapping("/add")
+    public ResponseEntity<Void> addProductPrice(@RequestBody ProductPrice productPrice, UriComponentsBuilder builder) {
+        boolean flag = productPriceService.addProductPrice(productPrice);
+        if (!flag) return new ResponseEntity<>(HttpStatus.CONFLICT);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(builder.path("api/price").buildAndExpand(productPrice.getId()).toUri());
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductPrice> getProductPriceById(@PathVariable("id") String id) {
+        ProductPrice productPrice = productPriceService.getProductPriceById(id);
+        return new ResponseEntity<>(productPrice, HttpStatus.OK);
+    }
+}
