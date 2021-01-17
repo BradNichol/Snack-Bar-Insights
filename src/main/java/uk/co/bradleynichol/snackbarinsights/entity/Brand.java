8468b1d874d59c3c37898599bff416f514eb7ef8
Bradley Nichol <bradleynichol@live.co.uk>
2021-01-17 14:23:51 +0000

    OneTOMany annotation added to Brand, to add child product entity when calling endpoint.
    Had to use Jackson JsonIgnoreProperties as I was receiving duplicate JSON data.

--- src/main/java/uk/co/bradleynichol/snackbarinsights/entity/Brand.java
+++ src/main/java/uk/co/bradleynichol/snackbarinsights/entity/Brand.java
@@ -1,11 +1,14 @@
 package uk.co.bradleynichol.snackbarinsights.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Table(name = "brands")
 public class Brand {
 
@@ -17,6 +20,11 @@ public class Brand {
 
     @Column(name = "name")
     private String name;
+
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "brand")
+    @JsonIgnoreProperties("brand")
+    private List<Product> products;
 
     public Brand() {
     }
@@ -31,5 +39,9 @@ public class Brand {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Product> getProducts() {
+        return products;
     }
 }
